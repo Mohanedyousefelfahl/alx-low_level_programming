e05d7c3a6b736cf201ecd21cca34dd0bba6cfdaf
mohaned <mohanedelfahl3@gmail.com>
2023-03-16 16:18:30 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
- *
- * Return: Always 0
+ * Description: main - prints out a string to stdout
+ * Return: Always 0 if success
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
