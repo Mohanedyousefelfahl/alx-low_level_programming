5a0b9c5199f7260e4b513bd9437126d93a1fc054
mohaned <mohanedelfahl3@gmail.com>
2023-03-16 16:23:50 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- * Description: main - prints out a string to stdout
- * Return: Always 0 if success
+ * Description: main - prints out a string to stdout.
+ * Return: Always 0 if success.
  */
 int main(void)
 {
