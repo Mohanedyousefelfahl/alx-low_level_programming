8eb791ece6e72c8df46ef1a22481a193d871885e
mohaned <mohanedelfahl3@gmail.com>
2023-03-17 18:37:47 -0700

    Ds

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,3 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Lahcen Oubbaih
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,5 +27,6 @@ int main(void)
         {
                 printf("Last digit of %d is %d and is 0\n", n, n % 10);
         }
+
         return (0);
 }	
